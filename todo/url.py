71ba375c3854b5fb7ba3581d0df740b3e34b131a
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-03 13:49:35 +0300

    add task update view

--- todo/url.py
+++ todo/url.py
@@ -1,15 +1,16 @@
 from django.urls import path
 
-from todo.views import TagListView, TagCreateView, TagUpdateView, TagDeleteView, TaskLiskView, TaskCreateView
+from todo.views import TagListView, TagCreateView, TagUpdateView, TagDeleteView, TaskLiskView, TaskCreateView, \
+    TaskUpdateView
 
 urlpatterns = [
     path("", TaskLiskView.as_view(), name="task-list"),
     path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
-
 ]
 
 
