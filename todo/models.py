de90bf7348cef0c7276ecb72f0d6e3cc47a8d074
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-03 10:59:03 +0300

    create models and corresponging migrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(to=Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ['-completed', 'created_at']
