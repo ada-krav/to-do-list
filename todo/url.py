d81ae70c05a2acbbed54b73b8631b41d3735a0cf
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-03 13:52:43 +0300

    add task delete view

--- todo/url.py
+++ todo/url.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from todo.views import TagListView, TagCreateView, TagUpdateView, TagDeleteView, TaskLiskView, TaskCreateView, \
-    TaskUpdateView
+    TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path("", TaskLiskView.as_view(), name="task-list"),
     path("create/", TaskCreateView.as_view(), name="task-create"),
     path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
